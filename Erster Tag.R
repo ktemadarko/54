1c760c5fd1e6fd018e1f2f0abb8da18eb41bce0e
Ama Owusu-Darko <aowusuda@asu.edu>
2019-11-25 20:25:03 +0000

    Ich mag lernen

--- Erster Tag.R
+++ Erster Tag.R
@@ -36,21 +36,111 @@ z1<-list(1,2,"a","b")
 
 mat1<-matrix(c(1,2,3,4,5,6),nrow=2, ncol=3)
 
-#dataframe ====
+#dataframe and  ====
 #like matrices but different data types with headers
 #headers accessed with $
 df=as.data.frame(mat1)
-df1=data.frame(Name=c("Joyce B", "Ebenezer S"), Yrs=c(3,2))
+df1=data.frame(Name=c("Peter A","Joyce B", "Ebenezer S","Mike O"), Yrs=c(4,3,2,4), Age=c(64,65,66,64))
+df1$Gender=as.factor(c('m', 'f', "m", "m"))
+
+#plot----
+p1<-function(x1,y1){
+  p2<-plot(x1,y1)
+  return (p2)}
+
+p1(df1$Name,df1$Yrs)
+
+p1(df1$Name,df1$Age)
+
+p1(df1$Gender,df1$Age)
+
+p1(wb1,df3$Freq)
+
+str(df1$Gender)
+str(df3$Brood_ID)
+str(wb$Brood)
+wb1<-as.factor(wb$Brood)
+str(wb1)
+
+plot(as.factor(df1$Gender),df1$Age)
+#p1+ pch=22 col=rgb(0.2,0.4,0.6)
+
+#subsets====
+sub5=df1[df1$Yrs<=4 & df1$Yrs>2,]
+#sub3=df1[2<df1$Yrs,] the same sub4=df1[df1$Yrs>2,]
+
 
 #class====
 class(df)
 str(df)
 data<-read.csv("C:/Users/SunDevil/Desktop/ItRaSA_Accra_2019/Data/fields.txt", sep="\t")
+sub1=subset(data,data$Species_richness==30)
+
+?pch
 
 #tree====
 library("ape")
 MyTree=read.tree("C:/Users/SunDevil/Desktop/ItRaSA_Accra_2019/Data/MyTree.tre")
 plot.phylo(MyTree)
 
-head()
+#head()
 
+#Exercise 1----
+ex1<-read.csv("C:/Users/SunDevil/Desktop/ItRaSA_Accra_2019/Leaftraits.txt", sep="\t")
+#there  are 2548 observations in the data set----
+str(ex1)
+
+any(is.na(ex1$LMA))
+View(ex1$LMA)
+summary(ex1$LMA)
+
+#There are 178 NA's----
+summary(ex1$GF)
+str(ex1$GF)
+#there are 7 types of GF----
+subGF<-ex1[ex1$GF=="T",]
+
+str(subGF)
+str(ex1[ex1$GF=="T",])
+
+#a1<-aggregate(subGF,)
+
+#Ex3----
+wb<-read.csv("C:/Users/SunDevil/Desktop/ItRaSA_Accra_2019/Data/wingbowl.txt",sep="\t")
+t1<-table(wb$Brood)
+t1
+str(t1)
+df3<-as.data.frame(t1)
+
+colnames(df3)[1]="Brood_ID"
+
+barplot(df3$Freq, ylim=c(0,14), ylab= "Frequency",
+         xlab= "Brood_ID", main = "A bargraph showing the frequency of different Brood IDS", 
+        names.arg = c("301", "302", '303',' 305',' 306',' 307' ,'308',' 310', '311',' 313', '314', '315', '316', '317', '318 ','319',' 320' ,'321', '322', '323',' 324', '325', '326', '328' ))
+box(col=rgb(0.2,0.4,0.6))
+
+summary(wb1)
+summary(wb$Brood)
+#boxplot----
+boxplot(wb$Brood,names = "Brood", main="A boxplot showing the distribution of Brood lengths", col=rgb(0.9,0.7,0.5))
+box(col=rgb(0.5,0.3,0.1))
+str(df3$Brood_ID)
+
+?boxplot
+?barplot
+
+str(wb$Brood)
+summary(wb$Brood)
+levels(wb$Brood)
+length(which(wb$Brood))
+
+length(wb$Brood)
+
+length(wb$Brood==301)
+length(wb$Brood==302)
+
+
+plot(wb$Brood)
+summary(wb)
+
+str(wb)
